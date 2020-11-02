0894632cc358beaf5a17695745adefe3312b71f8
Felipe Rodrigues <felipersdf@hotmail.com>
2020-11-01 23:09:41 -0300

    refactor: refactor button & input

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,10 @@
+import { Field } from 'formik';
+import styled from 'styled-components';
+
+export const Container = styled(Field)`
+  padding: 10px;
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  border: 1px solid #232129;
+`
