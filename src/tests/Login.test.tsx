bbf2473bc942195f797458d9672de110598d0c2e
Larissa Gusmao <larissamgusmao@gmail.com>
2020-11-08 13:32:05 -0300

    fix: fix test to work with i18n

--- src/tests/Login.test.tsx
+++ src/tests/Login.test.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
 import Login from '../pages/Login';
+import i18n from '../config/i18n';
 
 describe('Login Form', () => {
+  i18n.changeLanguage('pt');
+
   test('should show errors and change values', async () => {
     const { getByPlaceholderText, getByTestId, getByText } = render(
-      <BrowserRouter>
-        <Login />
-      </BrowserRouter>,
+      <I18nextProvider i18n={i18n}>
+        <BrowserRouter>
+          <Login />
+        </BrowserRouter>
+      </I18nextProvider>,
     );
 
-    const emailInput = getByPlaceholderText('Seu email');
-    const passwordInput = getByPlaceholderText('Sua senha');
+    const emailInput = getByPlaceholderText('E-mail');
+    const passwordInput = getByPlaceholderText('Senha');
     const submitButton = getByTestId('submit');
 
     fireEvent.click(submitButton);
 
-    await waitFor(() => getByText('*Email is required'));
-    await waitFor(() => getByText('*Password is required'));
+    await waitFor(() => getByText('*Campo de e-mail obrigatório'));
+    await waitFor(() => getByText('*Campo de senha obrigatório'));
 
     fireEvent.change(emailInput, { target: { value: 'email@test.com' } });
     fireEvent.change(passwordInput, { target: { value: 'myPassword' } });
